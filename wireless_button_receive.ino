4dd5178e0425ee29c4b6cf0e88d150910475dccf
Andrew Kleindolph <akleindolph@gmail.com>
2025-03-21 12:58:37 -0700

    Update wireless_button_receive.ino

--- wireless_button_receive.ino
+++ wireless_button_receive.ino
@@ -2,13 +2,21 @@
 #include <SPI.h>  // Not actually used but needed to compile
 
 #define RELAY_PIN 9  // Pin connected to relay
+#define LED_PIN 8  
 #define RF_RECEIVER_PIN 2  // Data pin from RF receiver
+
 
 RH_ASK driver;
 
 void setup() {
     pinMode(RELAY_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);  // Internal pull-up
     pinMode(RF_RECEIVER_PIN, INPUT);
+    digitalWrite(RELAY_PIN, LOW);  // Turn relay OFF
+    digitalWrite(LED_PIN, HIGH);
+    delay(300);
+    digitalWrite(LED_PIN, LOW);
+
     
     Serial.begin(9600);
     Serial.println("Start");
@@ -30,8 +38,10 @@ void loop() {
         if (strcmp((char*)buf, "Hi") == 0) {  // Compare strings
             Serial.println("it works");
             digitalWrite(RELAY_PIN, HIGH); // Turn relay ON
+            digitalWrite(LED_PIN, HIGH);
             delay(5000);
             digitalWrite(RELAY_PIN, LOW);  // Turn relay OFF
+            digitalWrite(LED_PIN, LOW);
         }
     }
 }
